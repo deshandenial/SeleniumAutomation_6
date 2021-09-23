af852ccb45a9abbc1c579a5de4a1435de718f108
deshanraj <deshancse@gmail.com>
2021-09-23 13:01:42 +0530

    done

---
+++ src/main/java/com/qa/driver/Driver.java
@@ -0,0 +1,54 @@
+package com.qa.driver;
+
+import java.net.URL;
+import java.util.Objects;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.qa.constant.FrameworkConstant;
+import com.qa.enums.ConfigProperties;
+import com.qa.utils.PropertiesUtils;
+
+
+
+public final class Driver 
+{
+	private Driver()
+	{
+
+	}
+	public static void initDriver() throws Exception
+	{
+		if(Objects.isNull(DriverManager.getDriver()))
+		{
+			
+			System.setProperty("webdriver.chrome.driver",FrameworkConstant.getChromeDriverPath());
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			
+			DriverManager.setDriver(new ChromeDriver(options));
+			
+			/*
+			 * DesiredCapabilities capabilities = new DesiredCapabilities();
+			 * capabilities.setBrowserName("chrome");
+			 * capabilities.setPlatform(Platform.LINUX); 
+			 * URL url = new URL("http://localhost:4444/wd/hub"); DriverManager.setDriver(new
+			 * RemoteWebDriver(url,capabilities));
+			 */
+			DriverManager.getDriver().get(PropertiesUtils.get(ConfigProperties.URL));
+		}
+	}
+	public static void tearDown()
+	{
+		if(Objects.nonNull(DriverManager.getDriver()))
+		{
+			DriverManager.getDriver().close();
+			DriverManager.unload();
+		}
+	}
+}
---
+++ src/test/java/com/tbm/automation/BaseClass.java
@@ -0,0 +1,36 @@
+package com.tbm.automation;
+
+import java.util.Map;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import com.qa.driver.Driver;
+
+
+
+public class BaseClass 
+{
+	protected BaseClass()
+	{
+		
+	}
+	
+
+	
+	@BeforeMethod
+	public void beforeMethod() throws Exception
+	{
+		
+		Driver.initDriver();
+	}
+	
+	@AfterMethod
+	public void afterMethod()
+	{
+		Driver.tearDown();
+		
+	}
+	
+
+}
